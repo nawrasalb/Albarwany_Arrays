d1399997783f91a603a156364b45d0011b18ae43
Nawras Albarwani <nawras@mail.usf.edu>
2020-02-06 18:00:40 -0500

    input comments

--- Albarwany_Arrays/Program.cs
+++ Albarwany_Arrays/Program.cs
@@ -9,20 +9,16 @@ namespace Albarwany_Arrays
     {
         static void Main(string[] args)
         {
-            // produces an auto populated array with 25 elements
+            // produces an  array with 25 elements
             int[] elements = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
 
 
             // for loop to create iteration 
             for(int i =0 ;i < elements.Length; i++)
             {
-               
+               // displays message " element value 
                 Console.WriteLine(" Element value = " + elements[i]);
             }
-
-
-
-
 
         }
     }
