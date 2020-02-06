b8c1b9a2259a6bb863f92de2e1d0a1c42b6adaa3
Nawras Albarwani <nawras@mail.usf.edu>
2020-02-06 17:56:28 -0500

    Add project files.

---
+++ Albarwany_Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿// Author : Nawras Albarwany
+// Date : 2/6/2020
+// Demonstrates the use of an Array 
+using System;
+
+namespace Albarwany_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // produces an auto populated array with 25 elements
+            int[] elements = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+
+
+            // for loop to create iteration 
+            for(int i =0 ;i < elements.Length; i++)
+            {
+               
+                Console.WriteLine(" Element value = " + elements[i]);
+            }
+
+
+
+
+
+        }
+    }
+}
